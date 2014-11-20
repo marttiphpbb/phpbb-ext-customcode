5567a27b4affe5645745bc41bfaae63934f031fc
WiMaPe <WiMaPe@users.noreply.github.com>
2014-11-20 19:55:44 +0100

    Update common.php
    fixed type line 37 :
    
     'ACP_CUSTOMCODE_INCLUDE_EXAMPLE'			=> 'To include your own croeated files, prepend the filename with <code>../../../../../../customcode/</code><p><code>&lt;!-- INCLUDE ../../../../../../customcode/my_file.html --></code></p>',

--- language/en/common.php
+++ language/en/common.php
@@ -34,7 +34,7 @@ $lang = array_merge($lang, array(
 
 	'ACP_CUSTOMCODE'							=> 'Custom Code',
 	'ACP_CUSTOMCODE_EDIT'						=> 'Edit custom file',
-	'ACP_CUSTOMCODE_INCLUDE_EXAMPLE'			=> 'To include your own croeated files, prepend the filename with <code>../../../../../../customcode/</code><p><code>&lt;!-- INCLUDE ../../../../../../customcode/my_file.html --></code></p>',
+	'ACP_CUSTOMCODE_INCLUDE_EXAMPLE'			=> 'To include your own created files, prepend the filename with <code>../../../../../../customcode/</code><p><code>&lt;!-- INCLUDE ../../../../../../customcode/my_file.html --></code></p>',
 	'ACP_CUSTOMCODE_INCLUDECSS_EXAMPLE'			=> 'Include your created stylesheet in overall_header_head_append.html <p><code>&lt;!-- INCLUDECSS ../../../../../../customcode/my_file.css --></code></p>',
 	'ACP_CUSTOMCODE_INCLUDEJS_EXAMPLE'			=> 'To include a Javascript file that makes use of jQuery, use <code>INCLUDEJS</code> anywhere i.e. in overall_footer_after.html <p><code>&lt;!-- INCLUDEJS ../../../../../../customcode/my_file.js --></code></p>',
 	'ACP_CUSTOMCODE_CREATE_DELETE'				=> 'Create or delete custom files',
