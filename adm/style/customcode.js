5358f38367efe0b4b544ef9815c4925a34784665
martti <info@martti.be>
2014-11-27 11:34:57 +0100

    focus on create submit button when enter

--- adm/style/customcode.js
+++ adm/style/customcode.js
@@ -1,12 +1,18 @@
 ;(function($, window, document) {
 	$('document').ready(function () {
-		$('#editor_rows').bind('change keyup', function () {
+		$('input#editor_rows').bind('change keyup', function () {
 			var editor_rows = Math.max(Math.min(this.value, 999), 5);
 			$('#file_data').attr('rows', editor_rows);
 		});
 
 		$('select#filename').change(function () {
 			$('form#file_edit').submit();
+		});
+		
+		$('input#new_file').keydown(function(e) {
+			if (e.which == 13) {
+				$('input#create').focus();
+			}	
 		});
 	});
 })(jQuery, window, document);
