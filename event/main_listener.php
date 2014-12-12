24f0dd0778c0f41d6beb5c7c9ff72491a855106f
martti <info@martti.be>
2014-12-12 22:15:28 +0100

    persistent show_customcode_events parameter

--- event/main_listener.php
+++ event/main_listener.php
@@ -7,6 +7,10 @@
 
 namespace marttiphpbb\customcode\event;
 
+
+use phpbb\request\request;
+use phpbb\template\twig\twig as template;
+
 /**
 * @ignore
 */
@@ -17,18 +21,31 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 */
 class main_listener implements EventSubscriberInterface
 {
+	/* @var request */
+	protected $request;
+	
+	/* @var template */
+	protected $template;
 
 	/**
+	 * @param request $request
+	 * @param template $template
 	*/
 	public function __construct(
+		request $request,
+		template $template
 	)
 	{	
+		$this->request = $request;
+		$this->template = $template;
 	}
 
 	static public function getSubscribedEvents()
 	{
 		return array(
 			'core.user_setup'		=> 'load_language_on_setup',
+			'core.page_footer'		=> 'core_page_footer',
+			'core.append_sid'		=> 'core_append_sid',
 		);
 	}
 
@@ -40,5 +57,32 @@ class main_listener implements EventSubscriberInterface
 			'lang_set' => 'common',
 		);
 		$event['lang_set_ext'] = $lang_set_ext;
+	}
+	
+	public function core_page_footer($event)
+	{
+		$show_customcode_events = ($this->request->variable('show_customcode_events', 0)) ? true : false;
+		$this->template->assign_var('S_SHOW_CUSTOMCODE_EVENTS', $show_customcode_events);		
+	}
+	
+	public function core_append_sid($event)
+	{
+		if ($this->request->variable('show_customcode_events', 0))
+		{
+			$params = $event['params'];
+			if (is_string($params))
+			{
+				$params .= '&show_customcode_events=1';
+			}
+			else
+			{
+				if ($params === false)
+				{
+					$params = array();
+				}
+				$params['show_customcode_events'] = 1;
+			}
+			$event['params'] = $params;
+		}
 	}
 }
