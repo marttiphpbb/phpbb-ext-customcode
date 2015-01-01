9918783ee5c7e826e00c7f4d7c6355640324f0fc
martti <info@martti.be>
2015-01-01 09:54:25 +0100

    mv main_listener listener

--- event/listener.php
+++ event/listener.php
@@ -22,7 +22,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 /**
 * Event listener
 */
-class main_listener implements EventSubscriberInterface
+class listener implements EventSubscriberInterface
 {
 	/* @var auth */
 	protected $auth;
