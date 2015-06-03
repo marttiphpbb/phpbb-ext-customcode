fab7beb8b366b77e9f37c1b2bb037674f36ba811
Mauron <Mauron@users.noreply.github.com>
2015-06-03 16:18:11 +0200

    Italian translation for common.php

---
+++ language/it/common.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+* phpBB Extension - marttiphpbb customcode
+* @copyright (c) 2014 marttiphpbb <info@martti.be>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+
+$lang = array_merge($lang, array(
+
+	'CUSTOMCODE_HIDE_TEMPLATE_EVENTS_LOCATIONS'	=> 'Nascondi le posizioni degli eventi template di Custom code',
+	'CUSTOMCODE_INSIDE_HTML_HEAD'				=> '(nell’<code>head</code> dell’html)',
+	'CUSTOMCODE_GITHUB_LINK'					=> '%1$sLink Github a Custom code%2$s%3$sCustom code%4$s, estensione per phpBB',
+		// CUSTOMCODE_GITHUB_LINK: This is the example github link in the footer to be loaded on installation.
+		// Between %1$s and %2$s is a html comment. Between %3$s and %4$s is the link to the Custom Code repository on Github.
+));
