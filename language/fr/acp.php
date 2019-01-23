d1e13a5e2577660e0f2eac5520d1703b19a22e80
Raphaël M <Galixte@users.noreply.github.com>
2019-01-23 16:12:23 +0100

    Update of FRENCH translation for Custom Code extension

--- language/fr/acp.php
+++ language/fr/acp.php
@@ -1,15 +1,18 @@
 <?php
 
 /**
-*
-* Custom Code extension for the phpBB Forum Software package.
-* French translation by Galixte (http://www.galixte.com)
-*
-* @copyright (c) 2015 marttiphpbb <info@martti.be>
-* @license GNU General Public License, version 2 (GPL-2.0)
-*
-*/
+ *
+ * Custom Code. An extension for the phpBB Forum Software package.
+ * French translation by Galixte (http://www.galixte.com)
+ *
+ * @copyright (c) 2019 marttiphpbb <info@martti.be>
+ * @license GNU General Public License, version 2 (GPL-2.0-only)
+ *
+ */
 
+/**
+ * DO NOT CHANGE
+ */
 if (!defined('IN_PHPBB'))
 {
 	exit;
@@ -20,9 +23,25 @@ if (empty($lang) || !is_array($lang))
 	$lang = [];
 }
 
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
 $lang = array_merge($lang, [
-	'ACP_CUSTOMCODE_INCLUDE_EXAMPLE'			=> 'Pour inclure ses propres fichiers, ajouter aux noms des fichiers (fix this)',
-	'ACP_CUSTOMCODE_EXAMPLE_FILE'				=> 'my_file.html',
+	'ACP_CUSTOMCODE_INCLUDE_EXAMPLE'			=> 'Pour inclure ses propres fichiers, ajouter un préfixe aux noms des fichiers comme suit : <code>%1$s</code>.',
+	'ACP_CUSTOMCODE_EXAMPLE_FILE'				=> 'mon_fichier.html',
 	'ACP_CUSTOMCODE_CREATE_FILE'				=> 'Créer un fichier',
 	'ACP_CUSTOMCODE_DELETE'						=> 'Supprimer',
 	'ACP_CUSTOMCODE_DELETE_FILE_NAME'			=> 'Supprimer %s',
@@ -35,14 +54,14 @@ $lang = array_merge($lang, [
 	'ACP_CUSTOMCODE_SAVE_CONFIRM'				=> 'Confirmer la sauvegarde du fichier %s ?',
 	'ACP_CUSTOMCODE_SAVE'						=> 'Sauvegarder',
 	'ACP_CUSTOMCODE_SAVE_PURGE_CACHE'			=> 'Sauvegarder puis vider le cache',
-	'ACP_CUSTOMCODE_SAVE_PURGE_CACHE_CONFIRM'	=> 'Voulez-vous sauvegarder le fichier %s et vider le cache ?',
+	'ACP_CUSTOMCODE_SAVE_PURGE_CACHE_CONFIRM'	=> 'Confirmer la sauvegarde du fichier %s et la purge du cache.',
 	'ACP_CUSTOMCODE_FILE_SAVED'					=> 'Le fichier %s a été sauvegardé avec succès !',
-	'ACP_CUSTOMCODE_FILE_SAVED_CACHE_PURGED'	=> 'Le fichier %s a été sauvegardé et le cache a été vidé avec succès !',
+	'ACP_CUSTOMCODE_FILE_SAVED_CACHE_PURGED'	=> 'Le fichier %s a été sauvegardé et le cache a été purgé avec succès !',
 	'ACP_CUSTOMCODE_FILE_CREATED'				=> 'Le fichier %s a été créé.',
 	'ACP_CUSTOMCODE_FILENAME_EMPTY'				=> 'Le nom du fichier était vide.',
 	'ACP_CUSTOMCODE_FILE_NOT_CREATED'			=> 'Le fichier %s n’a pas pu être créé.',
 	'ACP_CUSTOMCODE_FILE_ALREADY_EXISTS'		=> 'Le fichier %s existe déjà.',
-	'ACP_CUSTOMCODE_DELETE_FILE_CONFIRM'		=> 'Supprimer le fichier %s ?',
+	'ACP_CUSTOMCODE_DELETE_FILE_CONFIRM'		=> 'Confirmer la suppression du fichier %s.',
 	'ACP_CUSTOMCODE_FILE_DELETED'				=> 'Le fichier %s a été supprimé.',
 	'ACP_CUSTOMCODE_FILE_DOES_NOT_EXIST'		=> 'Le fichier %s n’existe pas.',
 	'ACP_CUSTOMCODE_FILE_NOT_DELETED'			=> 'Impossible de supprimer le fichier %s.',
--- language/fr/info_acp_customcode.php
+++ language/fr/info_acp_customcode.php
@@ -1,15 +1,18 @@
 <?php
 
 /**
-*
-* Custom Code extension for the phpBB Forum Software package.
-* French translation by Galixte (http://www.galixte.com)
-*
-* @copyright (c) 2015 marttiphpbb <info@martti.be>
-* @license GNU General Public License, version 2 (GPL-2.0)
-*
-*/
+ *
+ * Custom Code. An extension for the phpBB Forum Software package.
+ * French translation by Galixte (http://www.galixte.com)
+ *
+ * @copyright (c) 2019 marttiphpbb <info@martti.be>
+ * @license GNU General Public License, version 2 (GPL-2.0-only)
+ *
+ */
 
+/**
+ * DO NOT CHANGE
+ */
 if (!defined('IN_PHPBB'))
 {
 	exit;
@@ -19,6 +22,22 @@ if (empty($lang) || !is_array($lang))
 {
 	$lang = [];
 }
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
 
 $lang = array_merge($lang, [
 	'ACP_CUSTOMCODE'							=> 'Code personnalisé',
